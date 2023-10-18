7714d0f8d835339390e830ee4abc5dd6addee01f
zhangzhen <zhen.zhang@shopee.com>
2023-10-18 18:05:12 +0800

    feat: lock page

---
+++ src/frontend/Lock/index.tsx
@@ -0,0 +1,96 @@
+// ProtectedPage.js
+import React, { useState, useEffect } from 'react';
+import "./index.module.scss"
+
+const ProtectedPage = () => {
+  const [code, setCode] = useState('');
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [attempts, setAttempts] = useState(0);
+
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const handleCodeSubmit = (event) => {
+    event.preventDefault();
+    // Replace 'yourCorrectCode' with your desired correct code
+    if (code === '18029jdkj!pakmsdkl1iopks!asda./') {
+      const expirationDate = new Date().getTime() + 7 * 24 * 60 * 60 * 1000; // Set expiration to 7 days from now
+      localStorage.setItem('authorized', JSON.stringify({ code, expirationDate }));
+      setIsAuthorized(true);
+    } else {
+      setAttempts((prevAttempts) => prevAttempts + 1);
+    }
+  };
+
+  useEffect(() => {
+    const authorizedData = localStorage.getItem('authorized');
+    if (authorizedData) {
+      const { code: storedCode, expirationDate } = JSON.parse(authorizedData);
+      if (expirationDate > new Date().getTime()) {
+        setCode(storedCode);
+        setIsAuthorized(true);
+      }
+    }
+
+    if (attempts >= 5) {
+      const expirationDate = new Date().getTime() + 24 * 60 * 60 * 1000; // Set expiration to 1 day from now
+      localStorage.setItem('limitExceeded', JSON.stringify({ expirationDate }));
+      setIsAuthorized(false);
+    }
+  }, [attempts]);
+
+  useEffect(() => {
+    const limitExceededData = localStorage.getItem('limitExceeded');
+    if (limitExceededData) {
+      const { expirationDate } = JSON.parse(limitExceededData);
+      if (expirationDate > new Date().getTime()) {
+        setIsAuthorized(false);
+      }
+    }
+  }, []);
+
+  if (isAuthorized) {
+    // Render the protected content here
+    return (
+      <div className="protected-page">
+        <h1>Welcome to my website!</h1>
+        {/* Protected content */}
+      </div>
+    );
+  }
+
+  const limitExceededData = localStorage.getItem('limitExceeded');
+  if (limitExceededData) {
+    const { expirationDate } = JSON.parse(limitExceededData);
+    if (expirationDate > new Date().getTime()) {
+      // Render the limit exceeded page here
+      return (
+        <div className="limit-exceeded-page">
+          <h1>Code Input Limit Exceeded</h1>
+          <p>You have exceeded the maximum number of code input attempts.</p>
+          <p>Please try again after 24 hours.</p>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="protected-page">
+      <h1>Welcome to my website!</h1>
+      {attempts < 5 ? (
+        <>
+          <p>Please enter the correct code to access the content:</p>
+          <form onSubmit={handleCodeSubmit}>
+            <input type="text" value={code} onChange={handleCodeChange} />
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      ) : (
+        <p>Code input limit exceeded. Please try again after 24 hours.</p>
+      )}
+    </div>
+  );
+};
+
+export default ProtectedPage;
